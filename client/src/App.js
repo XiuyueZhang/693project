3b47aa191acc818ebe747066333b9dc6bdfe5274
Xiuyue <zxy0722nn@gmail.com>
2023-07-28 09:37:56 +1200

    fix mode problem

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
 
 function App() {
-  const mode = useSelector((state) => state.mode);
+  const mode = useSelector((state) => state.settings.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
